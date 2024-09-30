cbf9e2f304dda7a264fdcbd23316d52e3303a9c7
cryptospider937 <mihir.mahakalkar@gmail.com>
2024-10-01 00:22:27 +0530

    page layout

---
+++ src/components/TodoApp.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import TodoList from "./TodoList";
+
+function TodoApp() {
+  return (
+    <div className="container p-2 mx-auto">
+      <div className="flex flex-row flex-wrap">
+        <main role="main" className="w-full pt-1 px-2">
+          <h1 className="mt-8 mb-8" id="home">
+            Todo App
+          </h1>
+          <div className="overflow-hidden rounded-[0.5rem] border bg-background shadow">
+            <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+              <div className="flex items-center justify-between space-y-2">
+                <div>
+                  <h2 className="text-2xl font-bold tracking-tight">
+                    Welcome back!
+                  </h2>
+                  <p className="text-muted-foreground">
+                    Here's a list of your tasks for this month!
+                  </p>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                </div>
+              </div>
+              <TodoList />
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default TodoApp;
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+const TodoList = () => {
+  return (
+    <div className='rounded-md border'>
+      <div className='relative w-full overflow-auto'>
+        TODO - TABLE
+      </div>
+    </div>
+  )
+}
+
+export default TodoList
